6b6eeb01de946cf0ff4aad3347783f8bd4a29a48
AkashKannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-01-10 17:40:46 +0530

    Project initialized from start.spring.io

---
+++ src/test/java/com/AkashKannapiran/JobApp/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AkashKannapiran.JobApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
