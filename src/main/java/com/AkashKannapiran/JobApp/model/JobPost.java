98b680ac8ff11622f70e4ae962de056df44725b3
AkashKannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-01-24 15:09:19 +0530

    Used Lombok and optimized the IDE and code for Lombok

--- src/main/java/com/AkashKannapiran/JobApp/model/JobPost.java
+++ src/main/java/com/AkashKannapiran/JobApp/model/JobPost.java
@@ -2,9 +2,7 @@ package com.AkashKannapiran.JobApp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
