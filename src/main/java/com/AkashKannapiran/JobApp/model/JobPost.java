61b4e8bd3503870fc58ac1e780d658b19cb3b3a3
AkashKannapiran <153989662+AkashKannapiran@users.noreply.github.com>
2025-01-24 12:51:32 +0530

    Used Lombok

--- src/main/java/com/AkashKannapiran/JobApp/model/JobPost.java
+++ src/main/java/com/AkashKannapiran/JobApp/model/JobPost.java
@@ -2,7 +2,9 @@ package com.AkashKannapiran.JobApp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.List;
 @Data
 @Component
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class JobPost {
     @Id
     private int postID;
@@ -18,34 +22,4 @@ public class JobPost {
     private int reqExp;
     private List<String> postTechStack;
 
-    public JobPost() {
-    }
-
-    public JobPost(int postID, String postProfile, String postDesc, int reqExp, List<String> postTechStack) {
-        this.postID = postID;
-        this.postProfile = postProfile;
-        this.postDesc = postDesc;
-        this.reqExp = reqExp;
-        this.postTechStack = postTechStack;
-    }
-
-    public int getPostID() {
-        return postID;
-    }
-
-    public String getPostProfile() {
-        return postProfile;
-    }
-
-    public String getPostDesc() {
-        return postDesc;
-    }
-
-    public int getReqExp() {
-        return reqExp;
-    }
-
-    public List<String> getPostTechStack() {
-        return postTechStack;
-    }
 }
--- src/main/java/com/AkashKannapiran/JobApp/repo/JobRepo.java
+++ src/main/java/com/AkashKannapiran/JobApp/repo/JobRepo.java
@@ -4,8 +4,6 @@ import com.AkashKannapiran.JobApp.model.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
